d451088447dceab4690d01cc6dbe8d523252487d
Max <zeller.maximilian@gmx.net>
2021-05-30 22:39:20 +0200

    amazon product price

--- chromefetch.py
+++ chromefetch.py
@@ -2,16 +2,18 @@ from bs4 import BeautifulSoup
 import requests
 
 header = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0'}
+
+
 """
 mainurl = 'https://www.amazon.de/s?k=Ipad'
-
-
 
 searchpade = requests.get(mainurl, headers=header)
 search = BeautifulSoup(searchpade.content, 'html.parser')
 item = search.find(class_='a-link-normal a-text-normal').get_attribute_list('href')[0]
 print(item)
 """
+
+
 item = '/Neues-Apple-iPad-Air-Wi-Fi-64-GB/dp/B08J6SHYHX'
 
 url = f'https://www.amazon.de{item}'
